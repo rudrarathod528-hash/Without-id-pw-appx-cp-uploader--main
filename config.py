3134c044284d440a509ff1a73c05a219e19699fa
rudrarathod528-hash <rudrarathod528@gmail.com>
2026-08-14 02:31:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQ
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 6764661699))
+#OWNER = int(os.environ.get("OWNER", ))
 
 #LOG = -1002159628443,
 
@@ -17,7 +17,7 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 # auth_chats = []
 
 #try:
-    #ADMINS=[6764661699]
+    #ADMINS=[]
     #for x in (os.environ.get("ADMINS", "6764661699").split()):
         #ADMINS.append(int(x))
 #except ValueError:
